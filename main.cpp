34e5a0342d1eac1822b21f69a043dee606ce6207
Kamil <kamilratajczyk00@gmail.com>
2023-07-21 11:35:30 +0200

    Add score display

--- main.cpp
+++ main.cpp
@@ -152,6 +152,26 @@ int main(){
     Paddle paddle2(740.0f, 325.0f, sf::Keyboard::Scancode::Up, sf::Keyboard::Scancode::Down);
     Ball ball;
 
+    sf::Font font;
+    if (!font.loadFromFile("font.ttf"))
+    {
+        std::cout << "Error while loading the font!" << std::endl;
+    }
+
+    std::string score1String = std::to_string(*paddle1.getScore());
+    std::string score2String = std::to_string(*paddle2.getScore());
+    score1String.append(score2String);
+
+    sf::Text scoreText;
+    scoreText.setFont(font);
+    scoreText.setString(score1String);
+    scoreText.setCharacterSize(50);
+    scoreText.setLetterSpacing(10);
+    scoreText.setFillColor(sf::Color::White);
+    scoreText.setStyle(sf::Text::Bold);
+    scoreText.setOrigin(scoreText.getGlobalBounds().width / 2, scoreText.getGlobalBounds().height / 2);
+    scoreText.setPosition(400.0f, 100.0f);
+
     window.setFramerateLimit(60);
     while(window.isOpen()){
         sf::Event event;
@@ -171,9 +191,14 @@ int main(){
         float dt = 3;
         ball.detectCollision(paddle1.getSprite().getPosition().y, paddle2.getSprite().getPosition().y, paddle1.getScore(), paddle2.getScore());
         ball.move(dt);
-        std::cout << *paddle1.getScore() << " | " << *paddle2.getScore() << std::endl;
+
+        std::string score1String = std::to_string(*paddle1.getScore());
+        std::string score2String = std::to_string(*paddle2.getScore());
+        score1String.append(score2String);
+        scoreText.setString(score1String);
 
         window.clear();
+        window.draw(scoreText);
         window.draw(paddle1.getSprite());
         window.draw(paddle2.getSprite());
         window.draw(ball.getSprite());
